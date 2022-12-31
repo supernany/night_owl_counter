c3d147bf8d4cbd718c0032309ca594c4d8bf5bb4
supernany <supernany@users.noreply.github.com>
2022-12-31 23:20:15 +0100

    Add files via upload

--- archives.sh
+++ archives.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-# encoding: utf-8
 
-LAST_MONTH=$(date --date="last month" +%Y-%m)
-MY_PATH=$(dirname "$0")
-LOG_PATH="$MY_PATH/log"
-ARC_PATH="$LOG_PATH/archives"
-[[ ! -d "$ARC_PATH" ]] && mkdir "$ARC_PATH"
-FILES=$(ls "$LOG_PATH")
-ARCHIVE="$ARC_PATH/$LAST_MONTH.tar.gz"
-FILES_TO_ARCHIVE=$(echo "$FILES" | grep "$LAST_MONTH\-")
-echo "$FILES_TO_ARCHIVE"
-cd "$LOG_PATH" || exit
-tar -zcf "$ARCHIVE" $FILES_TO_ARCHIVE
-rm $FILES_TO_ARCHIVE
+Last_Month=$(date --date="last month" +%Y-%m)
+My_Path=$(dirname "$0")
+Log_Path="$My_Path/log"
+Arc_Path="$Log_Path/archives"
+[[ ! -d "$Arc_Path" ]] && mkdir "$Arc_Path"
+Files=$(ls "$Log_Path")
+Archive="$Arc_Path/$Last_Month.tar.gz"
+Files_To_Archive=$(echo "$Files" | grep "$Last_Month\-")
+# echo "$Files_To_Archive"
+cd "$Log_Path" || exit
+tar -zcf "$Archive" $Files_To_Archive
+rm $Files_To_Archive
